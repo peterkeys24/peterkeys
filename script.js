fcfc4b0ec825a92abef4d5537e110936969f792e
peterkeys24 <161449536+peterkeys24@users.noreply.github.com>
2024-04-20 16:47:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// const today =new Date(2024-03-03);
+
+// function countdown() {
+//     const now=new Date().getTime();
+//     const distance=birthday-now;
+
+//     const days= Math.floor(distance/(1000*60*60*24));
+//         const hours=Math.floor((distance% (1000*60*60*24)) /(1000*60*60));
+//         const minutes=Math.floor((distance%(1000*60*60)) /(1000*60));
+//         const seconds=Math.floor ((distance% (1000*60)) / 1000);
+
+//         document.getElementById('days').innerHTML= formatTime(days);
+//             document.getElementById('hours').innerHTML= formatTime(hours);
+//         document.getElementById('minutes').innerHTML= formatTime(minutes);
+//         document.getElementById('seconds').innerHTML= formatTime(seconds);
+//     }
+
+//     function formatTime(time) {
+//         return time< 10?'0${time}':time;
+//     }
+
+// countdown(
+    
+// );
+
+// setInterval(countdown,1000);
+alert('GOOD, EVENING!!');
+
+let ageBread=20;
+console.log(ageBread);
